6a06ff1e1a6a47a1bb381675e920890dea45237b
Yorgo Custers <417305@student.fontys.nl>
2022-04-11 21:43:32 +0200

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'lidltwitter.eu.auth0.com',
+      clientId: 'eV3BGa9w8WpzYzzKN3gh0YgfPN5CPa6v'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
